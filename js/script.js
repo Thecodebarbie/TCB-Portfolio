a004d23eff5498cc98d5e9a8c410ec2ee6958cfd
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-02-25 13:12:49 -0500

    Implement dynamic dark mode: - Add 'dark' class to the body element for styling changes. - Dynamically create and append a <style> element with custom dark mode styles. - Set id to 'darkModeStyles' for easy reference. - Update localStorage to indicate dark mode is enabled. - Implement disableDarkMode function to remove 'dark' class and associated styles when disabling dark mode.

--- js/script.js
+++ js/script.js
@@ -21,13 +21,33 @@ document.addEventListener('DOMContentLoaded', function () {
                         disableDarkMode();
                 }
         });
-        
         function enableDarkMode() {
                 document.body.classList.add('dark');
                 // Add dark mode styles dynamically
                 const styleElement = document.createElement('style');
                 styleElement.setAttribute('id', 'darkModeStyles');
                 styleElement.innerHTML = `
-            
- 
-        
+                  body.dark {
+                    --sidebar-color: #18191A;
+                    --primary-color: #242526;
+                    --primary-color-light: #3a3b3c;
+                    --arrow-color: #fae3ed;
+                    --toggle-color: #b0809a;
+                    --text-color: #ccc;
+                    --nav-link-color: #FBF6E9;
+                  }
+                `;
+    document.head.appendChild(styleElement);
+    localStorage.setItem('darkMode', 'enabled');
+  }
+
+  function disableDarkMode() {
+    document.body.classList.remove('dark');
+    // Remove dark mode styles dynamically
+    const darkModeStyles = document.getElementById('darkModeStyles');
+    if (darkModeStyles) {
+      darkModeStyles.remove();
+    }
+    localStorage.setItem('darkMode', null);
+  }
+});
