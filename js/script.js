786dccfa595c76f6fed0f2ca0721eca9ade35cfd
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-02-25 12:58:44 -0500

    added conditional statement ln 5 -15

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
         const darkModeToggle = document.getElementById('darkModeToggle');
         const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+        if (isDarkMode) {
+                enableDarkMode();
+              }
+            
+              darkModeToggle.addEventListener('change', () => {
+                if (darkModeToggle.checked) {
+                  enableDarkMode();
+                } else {
+                  disableDarkMode();
+                }
+              });
+            
+        
